a671de4b77fb30a9321765b9bc5f25b4a263fcf2
thamindu19 <92365822+thamindu19@users.noreply.github.com>
2023-01-18 17:38:43 +0530

    first commit

---
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,52 @@
+﻿// using Microsoft.AspNetCore.Mvc;
+
+// namespace CityInfo.API.Controllers
+// {
+//     [ApiController]
+//     [Route("api/cities")]
+//     public class CitiesController : ControllerBase
+//     {
+//         [HttpGet]
+//         public JsonResult GetCities()
+//         {
+//             return new JsonResult(CitiesDataStore.Current.Cities);
+//         }
+
+//         [HttpGet("{id}")]
+//         public JsonResult GetCity(int id)
+//         {
+//             return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+//         }
+//     }
+// }
+
+
+using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<CityDto>> GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCity(int id)
+        {
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityToReturn);
+        }
+    }
+}
---
+++ CityInfo/CityInfo.API/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.StaticFiles;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// File type provider
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Add endpoint routing
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Add endpoint routing
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.MapControllers();
+
+//app.Run(async (context) =>
+//{
+//    await context.Response.WriteAsync("Hello World!");
+//});
+
+app.Run();
